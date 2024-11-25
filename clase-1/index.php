ab4b0014b6d0734e31bc4523ee00329a7f2c5a8b
LucioHdz <luciohdz0@gmail.com>
2024-11-24 19:36:03 -0600

    Fin clase 1- variables

--- clase-1/index.php
+++ clase-1/index.php
@@ -1,6 +1,36 @@
 <?php
 
 
+//comentario inline
+
+#comentario inline
 
 
-?>
+/**
+ * comentario 
+ * multilinea
+ * 
+ * no se ejecuta
+ */
+
+
+/*
+    parseo:
+    - Revisión de la sintaxis
+    - Revisión logica
+
+    errores:
+    - warning     -> ALERTAS POSIBLEMENTE CRITICAS A FUTURO
+    - fatal error -> ERROR QUE  NO PERMITE CONTINUAR CON EL CODIGO
+    - notificación -> SOFT WARNING
+ 
+ */
+
+
+
+ $variable = "Mi muñeca me hablo";
+ echo "Hola Mundo";
+
+
+
+ ?>
---
+++ clase-1/prueba2.php
@@ -0,0 +1,9 @@
+<?php
+    $numero1=1;
+    $variableCamelCase = "Cualquier cosa";
+    // constante
+    define("CONSTANTE","valor de la constante");
+    echo CONSTANTE;
+    echo "  ";
+    echo $variableCamelCase;
+?>
